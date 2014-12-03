936f9b6e753c4764b8f56246a6f38683ffc460fc
h5appxya <h5app@outlook.com>
2014-12-03 18:16:45 +0800

    haha  all in one

---
+++ game/goto/game.js
@@ -0,0 +1,389 @@
+/**
+ * Created by gong on 2014/9/3.
+ */
+loop=false;//全局控制
+canvas_width = 320;
+canvas_height = 400;
+
+player_x=10;
+player_y=10;
+player_w=20;
+player_h=20;
+
+player_last_x=player_x;
+player_last_y=player_y;
+
+player_step=1;
+fps=1000/512;
+
+player_go_can_change=true;
+function player_save(){
+    player_last_x=player_x;
+    player_last_y=player_y;
+}
+function player_to_no(){
+}
+function player_to_left(){
+    if(loop){
+        player_save();
+        player_x-=player_step;
+        if(player_x<=0){
+            player_x=0;
+        }
+    }
+}
+function player_to_right(){
+    if(loop){
+        player_save();
+        player_x+=player_step;
+        if(player_x>=canvas_width-player_w){
+            player_x=canvas_width-player_w;
+        }
+    }
+}
+function player_to_up(){
+    if(loop){
+        player_save();
+        player_y-=player_step;
+        if(player_y<=0){
+            player_y=0;
+        }
+    }
+}
+function player_to_down(){
+    if(loop){
+        player_save();
+        player_y+=player_step;
+        if(player_y>=canvas_height-player_h){
+            player_y=canvas_height-player_h;
+        }
+    }
+}
+function player_to_back(){
+
+        //回到原来的地点
+        player_x=player_last_x;
+        player_y=player_last_y;
+    loop=true;
+
+    
+}
+function player(){
+    
+    c.drawImage(player_img,player_x,player_y,player_w,player_h);
+}
+function printTime(){
+    c.font="30px Georgia";
+    c.fillText(time,100,30);
+}
+
+function printBG(){
+    c.drawImage(bg_print,0,0,canvas_width,canvas_height);
+}
+function printBG_check(){
+    c.drawImage(bg_check,0,0,canvas_width,canvas_height);
+}
+function welcome(){
+    //隐藏正在载入的div
+    document.getElementById("loading").style.display="none";
+    //显示欢迎页div
+    document.getElementById("welcome").style.display="block";
+}
+function success(){
+    loop=false;
+    document.getElementById("success").style.display="block";
+    document.title="我"+time+"秒通过了智商测试，好开森，你行吗？";
+    document.getElementById("msg_success").innerHTML=document.title;
+}
+function gameover(){
+    loop=false;
+    document.getElementById("gameover").style.display="block";
+    document.title="我在第"+time+"秒挂掉了，没有通过智商测试，好桑心。。。";
+    document.getElementById("msg_gameover").innerHTML=document.title;
+}
+function restart(){
+    
+    time=0;
+    player_x=35;
+    player_y=92;
+	player_to=player_to_no;
+    loop=true;
+    player_go_can_change=true;
+    //隐藏正在载入的div
+    document.getElementById("loading").style.display="none";
+    //欢迎页隐藏
+    document.getElementById("welcome").style.display="none";
+    //隐藏gameover的div
+    document.getElementById("gameover").style.display="none";
+    //隐藏success的div
+    document.getElementById("success").style.display="none";
+}
+loadingResurcesNum=0;
+//判断资源是否载入
+function loadIsOk(){
+    if(loadingResurcesNum==4){
+        //已经载入 清除判断载入计时器
+        clearInterval(loadInterval);
+        //欢迎画面
+        welcome();
+    }
+}
+//载入资源
+function loadingResurces(){
+    player_img=new Image();
+    player_check_img=new Image();
+    bg_print=new Image();
+    bg_check=new Image();
+    player_img.onload = function() {
+        loadingResurcesNum++;
+       
+    }
+    player_check_img.onload = function() {
+        loadingResurcesNum++;
+        
+        c.clearRect(0, 0,canvas_width,canvas_height);
+        c.globalCompositeOperation = 'source-over';
+        c.drawImage(player_check_img,player_x,player_y,player_w,player_h);
+        player_data = c.getImageData(player_x,player_y,player_w,player_h).data;
+        
+    }
+    player_check_img.src="resoures/player_check.png";
+    bg_print.onload = function() {
+        loadingResurcesNum++;
+        
+    }
+    bg_check.onload = function() {
+        loadingResurcesNum++;
+        
+    }
+    
+    player_img.src="resoures/player.png";
+
+    bg_print.src="resoures/bg_print.png";
+    
+    bg_check.src="resoures/bg_check.png";
+}
+//初始化
+function init(){
+    time=0;
+    //canvas_width=document.body.scrollWidth;
+    //canvas_height=document.body.scrollHeight;
+    //canvas_width=document.getElementById("loading").scrollWidth;
+    //canvas_height=document.getElementById("loading").scrollHeight;
+    canvas=document.getElementById("myCanvas");
+
+    canvas.width=canvas_width;
+    canvas.height=canvas_height;
+    canvas.top = 0;
+    canvas.left = 0;
+    c = canvas.getContext('2d');
+    //
+
+    canvas.addEventListener('touchstart', function (e) {
+        if(loop){
+            var touchobj = e.changedTouches[0] // reference first touch point (ie: first finger)
+            startx = parseInt(touchobj.clientX) // get x position of touch point relative to left edge of browser
+            starty = parseInt(touchobj.clientY) // get x position of touch point relative to left edge of browser
+            //statusdiv.innerHTML = 'Status: touchstart<br /> ClientX: ' + startx + 'px'
+            e.preventDefault();
+        }
+    }, false);
+
+    canvas.addEventListener('touchmove', function (e) {
+        if(loop){
+            var touchobj = e.changedTouches[0] // reference first touch point for this event
+            
+            
+            //gameCtrl.player.x+=distX;
+            //gameCtrl.player.y+=distY;
+            //statusdiv.innerHTML = 'Status: touchmove<br /> Horizontal distance traveled: ' + dist + 'px'
+            //e.preventDefault();
+        }
+    }, false);
+
+    canvas.addEventListener('touchend', function (e) {
+        if(loop && player_go_can_change){
+            var touchobj = e.changedTouches[0] // reference first touch point for this event
+            //statusdiv.innerHTML = 'Status: touchend<br /> Resting x coordinate: ' + touchobj.clientX + 'px'
+            var distX = parseInt(touchobj.clientX) - startx;
+            var distY = parseInt(touchobj.clientY) - starty;
+			var len=40;
+            if (distX > len) {
+                player_to=player_to_right;
+            }
+            else if (distX < -len) {
+                player_to=player_to_left;
+            }
+            //startx = parseInt(touchobj.clientX);
+            
+            else if (distY > len) {
+                player_to=player_to_down;
+            }
+            else if (distY < -len) {
+                player_to=player_to_up;
+            }
+			player_go_can_change=false;
+			e.preventDefault();
+        }
+    }, false);
+    //载入资源
+    loadingResurces();
+    //判断资源是否已经载入
+    loadInterval=setInterval(loadIsOk,1000);
+}
+setInterval(function(){
+	if(loop){
+		time++;
+	}
+},1000);
+window.addEventListener("load",init,false);
+document.addEventListener("keypress", function (e) {
+    // body...
+    var keyCode = 0;
+    var e = e || window.event;
+    keyCode = e.keyCode || e.which || e.charCode;
+    //alert(keyCode);
+    if(loop){
+        if (keyCode == 109) {
+            //w
+        }
+        if (keyCode == 115) {
+            //s
+            player_to_down();
+            //showPlayer();
+        }
+        if (keyCode == 97) {
+            //a
+            player_to_left();
+            //showPlayer();
+        }
+        if (keyCode == 100) {
+            //d
+            player_to_right();
+            //showPlayer();
+        }
+        if (keyCode == 119) {
+            //w
+            player_to_up();
+            //showPlayer();
+        }
+        if (keyCode == 32) {
+            //fire
+        }
+    }
+    //
+}, false);
+
+var SHAKE_THRESHOLD = 3000;
+var last_update = 0;
+var x = y = z = last_x = last_y = last_z = 0;
+function deviceMotionHandler(eventData){
+        var acceleration = eventData.accelerationIncludingGravity;
+        //document.querySelector("#msg").innerHTML=acceleration;
+        var curTime = new Date().getTime();
+        if ((curTime - last_update) > 100) {
+            var diffTime = curTime - last_update;
+            last_update = curTime;
+            x = acceleration.x;
+            y = acceleration.y;
+            z = acceleration.z;
+            var speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
+
+            if (speed > SHAKE_THRESHOLD) {
+
+                num++;
+
+            }
+            //document.querySelector("#msg").innerHTML=x;
+            //重力感应
+            last_x = parseInt(x);
+            last_y = parseInt(y);
+            last_z = parseInt(z);
+        }
+}
+//window.addEventListener("devicemotion",deviceMotionHandler,false);
+function checking(){
+    
+    c.clearRect(0, 0,canvas_width,canvas_height);
+    printBG_check();
+    //以猪八戒为新的坐标获取data，对比原来的猪八戒图片，看像素是否重叠。
+    var data = c.getImageData(player_x,player_y,player_w,player_h).data;
+    //不每个像素都进行对比
+    var step=0;
+
+    for (var i = 3; i < data.length; i += 4) {
+        if(loop){
+            step++;
+            //每1个像素进行对比 速度提升0倍
+            if(step==1){
+                //
+                if (data[i]>0 && player_data[i] > 0) {
+                    //遇到障碍物
+					//console.log(data[i-3]+"-"+data[i-2]+"-"+data[i-1]);
+					player_go_can_change=true;
+                    if(data[i-3]==255 && data[i-2]==153 && data[i-1]==0){
+                       //alert('yellow'); 
+                        loop=false;
+                        player_to_back();
+                        console.log("yellow")
+                        console.log(player_y);
+                        console.log(player_last_y);
+                        
+                    }
+                    //遇到红色（敌人）悬崖等
+                    else if(data[i-3]==255 && data[i-2]==0 && data[i-1]==0){
+                       gameover();
+                        loop=false;
+                        //#f00
+                    }
+                    //遇到蓝色  回家 闯关成功 下一个关卡
+                    else if(data[i-3]==0 && data[i-2]==153 && data[i-1]==255){
+                       //success();
+                        //#f00
+                        success();
+                        loop=false;
+                    }
+
+                    break;
+                }
+                //
+                step=0;
+            }
+        }
+
+    }
+    c.clearRect(0, 0,canvas_width,canvas_height);
+    printBG();
+	
+    player();
+    printTime();
+    
+    //c.clearRect(0, 0,canvas_width,canvas_height);
+}
+function looping(){
+    if(loop){
+        //
+		/*
+        if(last_x>1){
+            //left
+            player_to_left();
+        }
+        if(last_x<-1){
+            //right
+            player_to_right();
+        }
+        if(last_y>1){
+            //left
+            player_to_down();
+        }
+        if(last_y<-1){
+            //right
+            player_to_up();
+        }*/
+        //
+        //c.clearRect(0, 0, canvas_width, canvas_height);
+		player_to();
+        checking();
+    }
+}
+setInterval(looping,fps);
---
+++ game/ufo/data.js
@@ -0,0 +1,64 @@
+var data={
+  "manifest_version": 2,
+  "name": "世界互联网大会召开，一大波IT大咖却被困火星，等你营救！",
+  "title": "IT大咖被困火星，等你营救！",
+  "description": "世界互联网大会召开，一大波IT大咖却被困火星，等你营救！",
+  "version": "1.0",
+  "designer": "许英剑",
+  "editor": "陈璐",
+  "author": "宫文博",
+  "copyright": "新京报新媒体",
+  "mans": [
+      /*
+      state:
+      0:排号中
+      1:等待飞行器
+      2:已经会去了
+      */
+      {"com":"腾讯","names":[{"name":"马化腾","value":1},{"name":"马云","value":0},{"name":"马布里","value":0}]},
+	  {"com":"阿里巴巴","names":[{"name":"马云","value":1},{"name":"马云","value":1},{"name":"马云","value":1}]},
+	  {"com":"百度","names":[{"name":"李泽楷","value":0},{"name":"李彦宏","value":1},{"name":"王力宏","value":0}]},
+	  {"com":"京东","names":[{"name":"罗永浩","value":0},{"name":"奶茶","value":0},{"name":"刘强东","value":1}]},
+	  {"com":"搜狐","names":[{"name":"陈羽凡","value":0},{"name":"张朝阳","value":1},{"name":"任志强","value":0}]},
+	  {"com":"奇虎360","names":[{"name":"周鸿祎","value":1},{"name":"周立波","value":0},{"name":"马化腾","value":0}]},
+	  {"com":"网易","names":[{"name":"张朝阳","value":0},{"name":"丁磊","value":1},{"name":"王中磊","value":0}]},
+	  {"com":"新浪","names":[{"name":"曹国伟","value":1},{"name":"丁磊","value":0},{"name":"陈彤","value":0}]},
+	  {"com":"58同城","names":[{"name":"姚劲波","value":1},{"name":"周立波","value":0},{"name":"吴秀波","value":0}]},
+	  {"com":"小米","names":[{"name":"刘强东","value":0},{"name":"雷军","value":1},{"name":"罗永浩","value":0}]},
+	  {"com":"猎豹","names":[{"name":"傅盛","value":1},{"name":"丁磊","value":0},{"name":"马化腾","value":0}]},
+	  {"com":"锤子","names":[{"name":"高晓松","value":0},{"name":"杜海涛","value":0},{"name":"罗永浩","value":1}]},
+	  {"com":"携程","names":[{"name":"周鸿祎","value":0},{"name":"范敏","value":1},{"name":"姚劲波","value":0}]},
+	  {"com":"facebook","names":[{"name":"扎克伯格","value":1},{"name":"库克","value":0},{"name":"盖茨","value":0}]},
+	  {"com":"盛大","names":[{"name":"马化腾","value":0},{"name":"陈天桥","value":1},{"name":"李彦宏","value":0}]},
+	  {"com":"谷歌","names":[{"name":"施密特","value":1},{"name":"乔布斯","value":0},{"name":"巴菲特","value":0}]},
+	  {"com":"苹果","names":[{"name":"盖茨","value":0},{"name":"库克","value":1},{"name":"扎克伯格","value":0}]},
+	  {"com":"去哪儿","names":[{"name":"姚劲波","value":0},{"name":"范敏","value":0},{"name":"庄辰超","value":1}]},
+	  {"com":"优酷土豆","names":[{"name":"古永锵","value":1},{"name":"曹国伟","value":0},{"name":"陈彤","value":0}]},
+	  {"com":"唯品会","names":[{"name":"陈欧","value":0},{"name":"雷军","value":0},{"name":"沈亚","value":1}]},
+	  {"com":"联想","names":[{"name":"杨元庆","value":1},{"name":"潘石屹","value":0},{"name":"李彦宏","value":0}]},
+	  {"com":"当当","names":[{"name":"刘强东","value":0},{"name":"李国庆","value":1},{"name":"杨元庆","value":0}]},
+	  {"com":"陌陌","names":[{"name":"唐岩","value":1},{"name":"李开复","value":0},{"name":"马化腾","value":0}]},
+	  {"com":"巨人网络","names":[{"name":"姚明","value":0},{"name":"任志强","value":0},{"name":"史玉柱","value":1}]},
+	  {"com":"亚马逊","names":[{"name":"贝佐斯","value":1},{"name":"张朝阳","value":0},{"name":"李开复","value":0}]},
+	  {"com":"凤凰网","names":[{"name":"曾毅","value":0},{"name":"玲花","value":0},{"name":"刘爽","value":1}]},
+	  {"com":"聚美优品","names":[{"name":"陈天桥","value":0},{"name":"陈彤","value":0},{"name":"陈欧","value":1}]},
+	  {"com":"爱奇艺","names":[{"name":"古永锵","value":0},{"name":"龚宇","value":1},{"name":"陈年","value":0}]},
+	  {"com":"hao123","names":[{"name":"李兴平","value":1},{"name":"雷军","value":0},{"name":"刘强东","value":0}]},
+	  {"com":"脸萌","names":[{"name":"乐嘉","value":0},{"name":"郭列","value":1},{"name":"罗永浩","value":0}]},
+	  {"com":"搜狗","names":[{"name":"王小川","value":1},{"name":"曹国伟","value":0},{"name":"潘石屹","value":0}]},
+	  {"com":"豆瓣","names":[{"name":"杨勃","value":1},{"name":"刘强东","value":0},{"name":"张朝阳","value":0}]},
+	  {"com":"知乎","names":[{"name":"任志强","value":0},{"name":"周源","value":1},{"name":"丁磊","value":0}]},
+	  {"com":"果壳","names":[{"name":"姬十三","value":1},{"name":"郭列","value":0},{"name":"李彦宏","value":0}]},
+	  {"com":"百合网","names":[{"name":"孟非","value":0},{"name":"乐嘉","value":0},{"name":"慕岩","value":1}]},
+      {"com":"凡客诚品","names":[{"name":"陈年","value":1},{"name":"陈天桥","value":0},{"name":"陈彤","value":0}]}
+  ],
+  "msg":[
+      {"from":0,"to":5,"msg":"你个菜鸟，赶紧去看新京报制作的世界互联网大会会刊!"},
+      {"from":6,"to":10,"msg":"呵呵，你是不是脸盲啊？"},
+      {"from":11,"to":20,"msg":"不错哇，你是不是也想做CEO呀！"},
+      {"from":20,"to":36,"msg":"哎呦！这么厉害，世界互联网大会没邀请你当嘉宾？"}
+  ],
+  "aboutUs": [
+    "url","http://www.bjnews.com.cn/"
+  ]
+}
